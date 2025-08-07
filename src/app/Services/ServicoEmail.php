785e416752b1e1fdd43c9a26f14757d566e17326
Jeferson Bruno Bezerra Silva <202412170010@ifto.local>
2025-08-07 20:49:15 -0300

    Envio de e-mail

--- src/app/Services/ServicoEmail.php
+++ src/app/Services/ServicoEmail.php
@@ -2,12 +2,50 @@
 
 namespace App\Services;
 
+//Importa as dependências
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
 class ServicoEmail {
     public static function enviar($destinatario, $assunto, $mensagem) {
-        $headers = "From: no-reply@vidaserena.com.br\r\n";
-        $headers .= "Reply-To: no-reply@vidaserena.com.br\r\n";
-        $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+             
+        //Verifica se o formulário foi enviado
+        // if(! isset($_POST['destinatario'])){
+        //     die("Informe o email para prosseguir com o envio"); 
+        // }
 
-        return mail($destinatario, $assunto, $mensagem, $headers);
+        // Obtém o host da variável de ambiente, ou 'localhost' como default
+        $rabbitmqHost = getenv('RABBITMQ_HOST') ?: 'localhost'; 
+
+        // Conexão com o RabbitMQ
+        $connection = new AMQPStreamConnection($rabbitmqHost, 5672, 'guest', 'guest');
+        $channel = $connection->channel();
+        $channel->queue_declare('fila_emails', false, true, false, false);
+
+        //Recebe os dados enviados pelo formulário
+        // $to = $destinatario;
+        // $subject = $titulo;
+        // $body = $mensagem;
+        // Dados do e-mail
+        $email = [
+            'to' => $destinatario,
+            'subject' => $assunto,
+            'body' => $mensagem
+        ];
+
+        //Converte os dados do email para JSON
+        $dados = json_encode($email);
+
+        //Gera a mensagem a ser inserida na fila
+        $msg = new AMQPMessage($dados, ['delivery_mode' => 2]); // persistente
+
+        //Publica a mensagem na fila
+        $channel->basic_publish($msg, '', 'fila_emails');
+
+        //Encerra a conexão
+        $channel->close();
+        $connection->close();
+
+        return true;
     }
 }
